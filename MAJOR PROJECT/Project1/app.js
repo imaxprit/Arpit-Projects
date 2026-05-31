c76720481e720b60100271ce10949734683337b5
arpit gupta <rajarpitgupta@gmail.com>
2026-05-31 16:32:53 +0530

    new edits

--- MAJOR PROJECT/Project1/app.js
+++ MAJOR PROJECT/Project1/app.js
@@ -26,6 +26,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.send("Hi, I am Root");
