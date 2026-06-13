3f75e557fc6647eb0c1c7f5bded3d58e7f19581e
arpit gupta <rajarpitgupta@gmail.com>
2026-06-13 12:32:13 +0530

    cookies

--- MAJOR PROJECT/Project1/classroom/server.js
+++ MAJOR PROJECT/Project1/classroom/server.js
@@ -2,8 +2,33 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js"); 
 const posts = require("./routes/post.js"); 
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser("secretcode"));    
+
+app.get("/getsignedcookie", (req, res) => {
+    res.cookie("made-In","INDIA", {signed: true});
+    res.send("signed cookies sent");
+});
+
+app.get("/verify", (req, res) => {
+    console.log(req.signedCookies);
+    res.send("verified");
+})
+
+app.get("/getcookies", (req, res) => {
+    res.cookie("great", "namaste");
+    res.cookie("madeIn","India");
+    res.send("sent you some cookies!");
+});
+
+app.get("/greet", (req, res) => {
+    let {name = "anonymus"} = req.cookies;
+    res.send(`Hi, ${name}`);
+});
 
 app.get("/", (req, res) => {
+    console.dir(req.cookies);   
     res.send("Hi, I am root");
 });
 
